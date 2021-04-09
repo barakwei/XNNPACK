f60ef3d26bcc8597ab0837e12fee5563ffd3487d
Barak Weiss <bweiss@lightricks.com>
2021-04-09 03:24:40 +0300

    build_ios_fat64: create a framework instead of a lib.

--- scripts/build-ios-fat64.sh
+++ scripts/build-ios-fat64.sh
@@ -6,7 +6,15 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-set -e
+set -ex
+
+if [ -z "$1" ]; then
+	echo "Output path must be provided"
+	exit 1
+fi
+
+OUT_DIR="$1"
+shift 1
 
 CMAKE_ARGS_COMMON=()
 
@@ -22,50 +30,66 @@ CMAKE_ARGS_COMMON+=("-DXNNPACK_BUILD_TESTS=OFF")
 
 # iOS-specific options
 CMAKE_ARGS_COMMON+=("-DENABLE_BITCODE=OFF")
-CMAKE_ARGS_COMMON+=("-DENABLE_ARC=OFF")
-CMAKE_ARGS_COMMON+=("-DDEPLOYMENT_TARGET=12.0")
+CMAKE_ARGS_COMMON+=("-DENABLE_ARC=ON")
+CMAKE_ARGS_COMMON+=("-DDEPLOYMENT_TARGET=13.0")
 CMAKE_ARGS_COMMON+=("-DENABLE_VISIBILITY=ON")
 
-# Use-specified CMake arguments go last to allow overridding defaults
+# Use-specified CMake arguments go last to allow overriding defaults
 CMAKE_ARGS_COMMON+=($@)
 
-#Build for device
-
+# Build for arm64 (device)
 CMAKE_ARGS_ARM=("${CMAKE_ARGS_COMMON[@]}")
 CMAKE_ARGS_ARM+=("-DPLATFORM=OS64")
 CMAKE_ARGS_ARM+=("-DIOS_ARCH=arm64")
 
-ARM_DIR=build/ios/arm64
+ROOT_DIR=$(pwd)
+
+INTERMEDIATE_DIR="${OUT_DIR}"/ios/intermediate
+mkdir -p "${INTERMEDIATE_DIR}"
+
+ARM_DIR="${OUT_DIR}"/ios/arm64
 mkdir -p "${ARM_DIR}"
 
-cd "${ARM_DIR}" && cmake ../../.. "${CMAKE_ARGS_ARM[@]}" && cmake --build . -- "-j$(nproc)"
-libtool -static -o libXNNPACK.a libXNNPACK.a clog/libclog.a cpuinfo/libcpuinfo.a pthreadpool/libpthreadpool.a
+echo "[+] Building for arm64"
+pushd "${ARM_DIR}"
+cmake "${ROOT_DIR}" "${CMAKE_ARGS_ARM[@]}"
+cmake --build . -- "-j$(sysctl -n hw.physicalcpu)"
+popd
 
-# Build for simulator
-CMAKE_ARGS_X86=("${CMAKE_ARGS_COMMON[@]}")
-CMAKE_ARGS_X86+=("-DPLATFORM=SIMULATOR64")
-CMAKE_ARGS_X86+=("-DIOS_ARCH=x86_64")
+echo "[+] Merging arm64 libraries"
+libtool -static -o "${INTERMEDIATE_DIR}"/libXNNPACK_arm64.a $(find ${ARM_DIR} -name "*.a")
 
-cd ../../..
+# Build for x64 (simulator)
+CMAKE_ARGS_X64=("${CMAKE_ARGS_COMMON[@]}")
+CMAKE_ARGS_X64+=("-DPLATFORM=SIMULATOR64")
+CMAKE_ARGS_X64+=("-DIOS_ARCH=x86_64")
 
-X86_DIR=build/ios/x86_64
-mkdir -p "${X86_DIR}"
+X64_DIR="${OUT_DIR}"/ios/x86_64
+mkdir -p "${X64_DIR}"
 
-cd "${X86_DIR}" && cmake ../../.. "${CMAKE_ARGS_X86[@]}" && cmake --build . -- "-j$(nproc)"
-libtool -static -o libXNNPACK.a libXNNPACK.a clog/libclog.a cpuinfo/libcpuinfo.a pthreadpool/libpthreadpool.a
+echo "[+] Building for x64"
+pushd "${X64_DIR}"
+cmake ../../.. "${CMAKE_ARGS_X64[@]}"
+cmake --build . -- "-j$(sysctl -n hw.physicalcpu)"
+popd
 
-# Prepare installation artefacts
-INSTALL_DIR=build/install
-INCLUDE_DIR="${INSTALL_DIR}"/include
-LIB_DIR="${INSTALL_DIR}"/lib
+echo "[+] Merging x64 libraries"
+libtool -static -o "${INTERMEDIATE_DIR}"/libXNNPACK_x86_64.a $(find ${X64_DIR} -name "*.a")
 
-cd ../../..
-mkdir -p "${INCLUDE_DIR}"
+# Prepare framework
+PRODUCT_DIR="${OUT_DIR}"/product
+FRAMEWORK_DIR="${PRODUCT_DIR}"/XNNPACK.framework
+INCLUDE_DIR="${FRAMEWORK_DIR}"/Headers
+LIB_DIR="${FRAMEWORK_DIR}"
+
 mkdir -p "${LIB_DIR}"
+mkdir -p "${INCLUDE_DIR}"
 
-lipo "${ARM_DIR}"/libXNNPACK.a "${X86_DIR}"/libXNNPACK.a -create -o "${LIB_DIR}"/libXNNPACK.a
+echo "[+] Creating universal staic lib"
+lipo "${INTERMEDIATE_DIR}"/libXNNPACK_arm64.a "${INTERMEDIATE_DIR}"/libXNNPACK_x86_64.a -create -o "${LIB_DIR}"/XNNPACK
 
+echo "[+] Copying Headers"
 cp include/xnnpack.h "${INCLUDE_DIR}"
 cp "${ARM_DIR}"/pthreadpool-source/include/pthreadpool.h "${INCLUDE_DIR}"
 
-echo "Build finished. Installation files can be found under ${INSTALL_DIR}."
+echo "[+] Build complete. The result framework can be found at ${PRODUCT_DIR}."
